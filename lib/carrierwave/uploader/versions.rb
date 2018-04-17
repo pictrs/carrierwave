925054368a3d69cddea0ef4af689acebd699958f
philipp <philipp.albig@pictrs.com>
2018-04-17 09:36:23 +0200

    cache! and store_versions! for a specific version
    Rebased custom feature from pictrs fork onto v2.2.6.
    Adds top_version method and selective version caching/storing.
    Integrates with v2.2.6's versions_to_store/recreate_versions! system.

--- lib/carrierwave/uploader/versions.rb
+++ lib/carrierwave/uploader/versions.rb
@@ -296,6 +296,16 @@ module CarrierWave
         parent_version.try(:versions) || []
       end
 
+      def top_version
+        pv = self
+        i = 0
+        while pv.parent_version
+          pv = pv.parent_version
+          raise 'versions are somehow unexpected nested' if (i += 1) > 20
+        end
+        pv
+      end
+
       def full_filename(for_file)
         [version_name, super(for_file)].compact.join('_')
       end
@@ -311,8 +321,14 @@ module CarrierWave
         end
       end
 
-      def store_versions!(new_file)
-        versions_to_store.each { |name, v| v.store!(new_file) }
+      def store_versions!(new_file, version_names = nil)
+        if version_names
+          versions_to_store.each do |name, v|
+            v.store!(new_file) if version_names.any? { |vn| vn.to_sym == name.to_sym }
+          end
+        else
+          versions_to_store.each { |name, v| v.store!(new_file) }
+        end
       end
 
       def remove_versions!
